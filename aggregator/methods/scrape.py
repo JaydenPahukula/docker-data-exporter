4812556afe927ce63ec4c452b55087721707c56d
JaydenPahukula <jayden.pahukula@gmail.com>
2023-07-17 20:19:40 -0400

    basic influxdb writing works

--- aggregator/methods/scrape.py
+++ aggregator/methods/scrape.py
@@ -7,15 +7,15 @@ BUCKET = "main"
 
 def scrape(ip:str):
 
+    # http get request
     response = get(f"http://{ip}/get-data")
     if response.status_code != 200:
         print(f"Something went wrong while scraping {ip}...\n", response.text)
         return ""
 
     data = response.json()
-
-    return (f"total-container-count:{data['total-container-count']}i, "
-            f"running-container-count:{data['running-container-count']}i")
+    return (f"total-container-count={data['total-container-count']}u,"
+            f"running-container-count={data['running-container-count']}u")
 
 
 
@@ -23,21 +23,23 @@ def scraper(iplist:list, intervalSeconds:int):
 
     interval = datetime.timedelta(seconds=intervalSeconds)
     last = datetime.datetime.now() - interval
-    print("hi")
+    
     while 1:
         curr = datetime.datetime.now()
         if curr - interval > last:
             last = curr
 
+            # scrape all data and format
             feilds = [scrape(ip) for ip in iplist]
             unixTime = int(time.mktime(curr.timetuple()))
             datapoints = [f"{iplist[i]} {feilds[i]} {unixTime}" for i in range(len(iplist))]
-
-            completedResponse = run(f"influx write -b {BUCKET} -p ms \"" + '\n'.join(datapoints) + "\"",
+            
+            # write to database
+            completedResponse = run(f"influx write --bucket {BUCKET} --precision s \"" + '\n'.join(datapoints) + "\"",
                                     capture_output=True, shell=True)
             if completedResponse.returncode != 0:
-                print("Error writing to database\n", completedResponse.stdout, "\n", completedResponse.stderr)
-            else:
-                print("Wrote data to database")
+                print("Error writing to database\n ", completedResponse.stdout.decode(), "\n ", completedResponse.stderr.decode())
+        
+        time.sleep(0.01)
 
     return
