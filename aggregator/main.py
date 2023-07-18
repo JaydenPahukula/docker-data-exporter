05a5c7b14b0a9a6a6e510dacaee9e1f30349085b
Jaxson Pahukula <jaxpahu@gmail.com>
2023-07-18 10:24:09 -1000

    add config file

--- aggregator/main.py
+++ aggregator/main.py
@@ -1,13 +1,16 @@
 import flask
 from flask_cors import CORS, cross_origin
+import os
 import sys
 import threading
+import yaml
 
 from methods.scrape import scraper
 from methods.handle_query import handle_query
 
-SCRAPEINTERVAL = 60 # seconds
-IPLIST = ["127.0.0.1:5001"]
+SCRAPE_INTERVAL = 600 # (seconds)
+IP_LIST = []
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = flask.Flask(__name__)
 cors = CORS(app)
@@ -31,17 +34,26 @@ def query():
 
 if __name__ == '__main__':
     print("\n\n\n")
+    
+    print(" * Reading config file")
+    with open(CURRENT_DIR + "/config.yaml", "r") as config_file:
+        config = yaml.safe_load(config_file)
+
+    SCRAPE_INTERVAL = config["scrape-interval"]
+    IP_LIST = config["server-ips"]
 
     print(" * Starting scraper thread")
-    logThread = threading.Thread(target=scraper, args=(IPLIST, SCRAPEINTERVAL), daemon=True)
-    logThread.start()
+    scraper_thread = threading.Thread(target=scraper, args=(IP_LIST, SCRAPE_INTERVAL), daemon=True)
+    scraper_thread.start()
 
     port = 5000 # default port
     # getting port argument
     for i in range(len(sys.argv)):
         if (sys.argv[i] == "-p" or sys.argv[i] == "-port") and i + 1 < len(sys.argv):
-            try:                port = int(sys.argv[i + 1])
-            except ValueError:  pass
-            finally:            break
+            try:
+                port = int(sys.argv[i + 1])
+            except ValueError:
+                pass
+            break
 
     app.run(port=port)
